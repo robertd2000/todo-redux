833627ee6db1dce65e6b14d1ebf3adc194f012c6
robertd2000 <zdabaghov@mail.ru>
2025-06-05 12:57:18 +0300

    TodoList

--- src/entities/todos/model/selectors.ts
+++ src/entities/todos/model/selectors.ts
@@ -1,7 +1,7 @@
 import type { RootState } from "../../../app/store";
 import type { TodoItemType } from "../types";
 
-export const selectAllTodos = (state: RootState) => state.todos;
+export const selectAllTodos = (state: RootState) => state.todos.todos;
 
 export const selectCompletedTodos = (state: RootState) =>
   state.todos.todos.filter((todo: TodoItemType) => todo.completed);
--- src/widgets/todo-list/ui/TodoList.tsx
+++ src/widgets/todo-list/ui/TodoList.tsx
@@ -1,5 +1,7 @@
 import type { Filter } from "../../../shared/types";
+import { Empty } from "../../../shared/ui";
 import { useTodoList } from "../model";
+import style from "../styles/TodoList.module.scss";
 
 interface Props {
   filter: Filter;
