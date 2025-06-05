ba3a813e0acbcf7a30b60197c4966f7130df320a
robertd2000 <zdabaghov@mail.ru>
2025-06-05 13:25:55 +0300

    CompleteTodo

---
+++ src/features/complete-todo/ui/index.tsx
@@ -0,0 +1,1 @@
+export { CompleteTodo } from "./CompleteTodo";
--- src/widgets/todo-item/ui/TodoItem.tsx
+++ src/widgets/todo-item/ui/TodoItem.tsx
@@ -1,10 +1,13 @@
 import type { TodoItemProps } from "../types";
+import { CompleteTodo } from "../../../features/complete-todo";
 import style from "../styles/TodoItem.module.scss";
 
 export function TodoItem({ todo }: TodoItemProps) {
   return (
     <div className={style.todo} id={todo.id}>
-      <div className={style.start}>{/* <CompleteTodo todo={todo} /> */}</div>
+      <div className={style.start}>
+        <CompleteTodo todo={todo} />
+      </div>
       <div className={style.middle}>{/* <EditTodo todo={todo} /> */}</div>
       <div className={style.end}>{/* <DeleteTodo id={todo.id} /> */}</div>
     </div>
